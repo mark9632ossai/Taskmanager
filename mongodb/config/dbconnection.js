2b2325c25bf75e67f2843e2ffed0e206b0fd2495
Mark Ossai <chiguy9632@gmail.com>
2025-03-12 09:32:06 +0000

    added the search bar and fixed depreciation error

--- mongodb/config/dbconnection.js
+++ mongodb/config/dbconnection.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const connectDB = async () => {
     try {
-        const connString = 'mongodb+srv://michellealex1604:nZ74iMDAtwGqYe7r@cluster0.bnu40.mongodb.net/taskmanager?retryWrites=true&w=majority';
-        const conn = await mongoose.connect(connString); // Remove deprecated options
+        // Use process.env.url or change the env variable name to MONGO_URI
+        const connString = process.env.url;
+        const conn = await mongoose.connect(connString);
         console.log(`Database connected on ${conn.connection.host}`);
     } catch (error) {
         console.error(`Connection to the database failed: ${error}`);
-        
     }
 };
